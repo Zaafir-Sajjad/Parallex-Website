a27aff8c93e1256acd8341f3bb11991d085aab8c
Zaafir-Sajjad <zaafirsajjad@gmail.com>
2024-10-12 19:11:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let age;
+age = window.prompt("enter your age");
+
+if(age<16){
+    document.getElementById("text-1bg").textContent =`Nigga Go Study You Have Papers`;
+}
+else if(age>16){
+    document.getElementById("text-1bg").textContent =`Nigga Go Study You Have MDCAT cuming`;
+}
+
+else{
+    document.getElementById("text-1bg").textContent = `enter age`; 
+}
